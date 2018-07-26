e26c50499f3a3d8d8b255c37e3c08e05ab8687b9
Daniel Grubbs <35300125+danielgrubbs@users.noreply.github.com>
2018-07-25 19:53:46 -0500

    Files for lesson 05 activity

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+import time
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/time/')
+def epoch_time():
+    epoch = int(time.time())
+    return str(epoch)
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 6738))
+    app.run(host='0.0.0.0', port=port)
